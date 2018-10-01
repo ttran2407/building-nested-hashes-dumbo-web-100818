3d8591a2b0d7d5652af24c59d906d53ed6e36d92
Tien Tran <tientran2407.vn@gmail.com>
2018-10-01 21:16:09 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patricard => {},
+      :patricarh => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
