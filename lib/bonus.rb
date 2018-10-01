88c2381e78ad834337bd5ce4d78626c26f1120b0
Tien Tran <tientran2407.vn@gmail.com>
2018-10-01 21:21:50 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy[:heroine][:status] = "dead"
-epic_tragedy[:hero][:status] = "dead"
+epic_tragedy[:montague][:heroine][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
 
   
 
